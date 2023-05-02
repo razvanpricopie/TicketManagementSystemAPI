7253efe26781b9a549d7ec0bb36d3c54691f646f
Razvan Alexandru Pricopie <Pricopie.Ma.Razvan@student.utcluj.ro>
2023-05-02 15:48:25 +0300

    infrastructure service registration added

---
+++ TicketManagementSystemAPI.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TicketManagementSystemAPI.Application.Contracts.Infrastructure;
+using TicketManagementSystemAPI.Application.Models.Mail;
+using TicketManagementSystemAPI.Infrastructure.Mail;
+
+namespace TicketManagementSystemAPI.Infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration) 
+        {
+            services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
+
+            services.AddTransient<IEmailService, EmailService>();
+
+            return services;
+        }
+    }
+}
