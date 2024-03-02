5a4ea3e0f0f6d012aff22f9f0d5e6787b93f1446
razvanpricopie <pricopie.razvan98@gmail.com>
2024-03-02 16:30:10 +0200

    minor change

--- TicketManagementSystemAPI.Api/Startup.cs
+++ TicketManagementSystemAPI.Api/Startup.cs
@@ -40,7 +40,7 @@ namespace TicketManagementSystemAPI.Api
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
